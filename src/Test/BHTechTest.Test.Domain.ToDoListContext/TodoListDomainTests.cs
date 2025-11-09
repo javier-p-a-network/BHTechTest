73b441e828be7c0869de3bf81da099b99ddd282a
javier_plaza <javier.plaza.network@gmail.com>
2025-11-09 19:22:20 +0100

    Fix unit tests

--- src/Test/BHTechTest.Test.Domain.ToDoListContext/TodoListDomainTests.cs
+++ src/Test/BHTechTest.Test.Domain.ToDoListContext/TodoListDomainTests.cs
@@ -33,7 +33,7 @@ namespace BHTechTest.Test.Domain.ToDoListContext
         }
 
         [TestMethod]
-        public void HappyPath_example_and_print_like_sample()
+        public void HappyPath_example()
         {
             // Arrange
             var repoMock = CreateDefaultRepoMock(nextId: 1, categories: new[] { "Work" });
@@ -45,26 +45,28 @@ namespace BHTechTest.Test.Domain.ToDoListContext
             var id = addResult.Value;
 
             var r30 = todoListService.RegisterProgression(id, new DateTime(2025, 3, 18), 30m);
-            var r80 = todoListService.RegisterProgression(id, new DateTime(2025, 3, 19), 80m);
-            var r100 = todoListService.RegisterProgression(id, new DateTime(2025, 3, 20), 100m);
+            var r50 = todoListService.RegisterProgression(id, new DateTime(2025, 3, 19), 50m);
+            var r20 = todoListService.RegisterProgression(id, new DateTime(2025, 3, 20), 20m);
+
+            var printResult = todoListService.PrintItems();
 
             var getResult = todoListService.GetItem(id);
             var item = getResult.Value;
 
             //Assert
             Assert.IsFalse(addResult.HasErrors, "AddItem should succeed");
-            Assert.IsFalse(r30.HasErrors, "RegisterProgression 30% should succeed");
-            Assert.IsFalse(r80.HasErrors, "RegisterProgression 80% should succeed");
-            Assert.IsFalse(r100.HasErrors, "RegisterProgression 100% should succeed");
+            Assert.IsFalse(r30.HasErrors, "RegisterProgression +30% should succeed");
+            Assert.IsFalse(r50.HasErrors, "RegisterProgression +50% should succeed");
+            Assert.IsFalse(r20.HasErrors, "RegisterProgression +20% should succeed");
             Assert.IsFalse(getResult.HasErrors, "GetItem should succeed");
             Assert.IsNotNull(item);
             Assert.IsTrue(item.IsCompleted, "Item should be completed after 100%");
             Assert.AreEqual(3, item.Progressions.Count);
-            Assert.AreEqual(100m, item.Progressions.Last().Percent);
+            Assert.AreEqual(100m, item.AcumulativeProgressPercent);
         }
 
         [TestMethod]
-        public void Cannot_add_progression_with_lower_or_equal_date()
+        public void Cannot_add_progression_with_lower_or_equal_date_but_greater_is_ok()
         {
             // Arrange
             var repoMock = CreateDefaultRepoMock(nextId: 1, categories: new[] { "Work" });
@@ -78,16 +80,18 @@ namespace BHTechTest.Test.Domain.ToDoListContext
             var first = todoListService.RegisterProgression(id, new DateTime(2025, 1, 1), 10m);
             var sameDate = todoListService.RegisterProgression(id, new DateTime(2025, 1, 1), 20m);
             var olderDate = todoListService.RegisterProgression(id, new DateTime(2024, 12, 31), 20m);
+            var newerDate = todoListService.RegisterProgression(id, new DateTime(2025, 1, 2), 20m);
 
             //Assert
             Assert.IsFalse(addResult.HasErrors);
             Assert.IsFalse(first.HasErrors);
             Assert.IsTrue(sameDate.HasErrors, "Registering a progression with the same date should produce an error");
             Assert.IsTrue(olderDate.HasErrors, "Registering a progression with an older date should produce an error");
+            Assert.IsFalse(newerDate.HasErrors, "Registering a progression with an newer date should succeed");
         }
 
         [TestMethod]
-        public void Cannot_register_progression_with_invalid_percent_or_non_increasing_percent()
+        public void Cannot_register_progression_with_invalid_percent()
         {
             //Arrange
             var repoMock = CreateDefaultRepoMock(nextId: 1, categories: new[] { "Work" });
@@ -101,14 +105,35 @@ namespace BHTechTest.Test.Domain.ToDoListContext
             var p0 = todoListService.RegisterProgression(id, DateTime.UtcNow.AddDays(1), 0m);
             var p120 = todoListService.RegisterProgression(id, DateTime.UtcNow.AddDays(1), 120m);
             var p30 = todoListService.RegisterProgression(id, DateTime.UtcNow.AddDays(1), 30m);
-            var p20 = todoListService.RegisterProgression(id, DateTime.UtcNow.AddDays(2), 20m);
 
             //Assert
             Assert.IsFalse(addResult.HasErrors);
             Assert.IsTrue(p0.HasErrors, "Percent 0 should be invalid");
             Assert.IsTrue(p120.HasErrors, "Percent > 100 should be invalid");
             Assert.IsFalse(p30.HasErrors, "Valid progression 30% should succeed");
-            Assert.IsTrue(p20.HasErrors, "Non-increasing percent should be invalid");
+        }
+
+        [TestMethod]
+        public void Cannot_register_progression_with_acummulative_percent_greater_than_100percent()
+        {
+            //Arrange
+            var repoMock = CreateDefaultRepoMock(nextId: 1, categories: new[] { "Work" });
+            var outputMock = CreateOutputMock();
+            var todoListService = new TodoListService(repoMock.Object, outputMock.Object);
+
+            //Act
+            var addResult = todoListService.AddItem("Task", "Desc", "Work");
+            var id = addResult.Value;
+
+            var rp1_30 = todoListService.RegisterProgression(id, DateTime.UtcNow.AddDays(1), 30m);
+            var rp2_80 = todoListService.RegisterProgression(id, DateTime.UtcNow.AddDays(2), 80m);
+            var rp2_70 = todoListService.RegisterProgression(id, DateTime.UtcNow.AddDays(2), 70m);
+
+            //Assert
+            Assert.IsFalse(addResult.HasErrors);
+            Assert.IsFalse(rp1_30.HasErrors, "Progression 0 + 30% should succeed");
+            Assert.IsTrue(rp2_80.HasErrors, "Progression 30 + 80 = 110% percent should be invalid");
+            Assert.IsFalse(rp2_70.HasErrors, "Progression 30 + 70 = 100% percent should succeed");
         }
 
         [TestMethod]
